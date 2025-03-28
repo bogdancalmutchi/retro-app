21e3f1796d4d9d5ce820f8f8bf83b02ca12714ce
Bogdan Calmutchi <bcalmutchi@intralinks.com>
2025-03-28 18:51:59 +0200

    oopsie

--- frontend/src/firebase.ts
+++ frontend/src/firebase.ts
@@ -1,19 +1,17 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBTwQvhGe79W3Lx3WGfGe0it8yyghVnWXc",
-  authDomain: "retro-app-a95ca.firebaseapp.com",
-  projectId: "retro-app-a95ca",
-  storageBucket: "retro-app-a95ca.firebasestorage.app",
-  messagingSenderId: "9788879482",
-  appId: "1:9788879482:web:6ada9a09c50149baa701c3"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app); // Add Firestore
+const db = getFirestore(app);
 
 export { db };
